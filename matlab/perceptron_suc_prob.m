7790fd4ffaf59caba88d88c4dff4df09ad09b5df
chi <zhang.chi9@husky.neu.edu>
2020-01-02 16:53:12 -0500

    update perceptron success probability

--- matlab/perceptron_suc_prob.m
+++ matlab/perceptron_suc_prob.m
@@ -1,9 +1,8 @@
-function perceptron_sub_prob(N,alpha)
+function perceptron_suc_prob(N,alpha,int_noise)
 
 addpath('/home/zhang.chi9/research/logscale/code/functions')
 RandStream.setGlobalStream(RandStream('mt19937ar','seed',sum(100*clock)));
 
-a = 40;
 rj = 2^-6;
 rout = rj;
 f = 0.2;
@@ -12,7 +11,7 @@ n_trials = 200;
 m = round(alpha*N*0.1872);
 
 W = nan(N,n_trials);
-routmin = nan(1,n_trials);
+routlast = nan(1,n_trials);
 
 create_parpool()
 parfor i=1:n_trials
@@ -22,8 +21,9 @@ parfor i=1:n_trials
     while sum(Xp==-1) == m
         Xp = 2*(rand(1,m)<f)-1;
     end
-    [W(:,i),routmin(i)] = perceptron_learning_with_noise(X,Xp,f,rj,a,rout);
+    %[W(:,i),routmin(i)] = perceptron_learning_with_noise(X,Xp,f,rj,a,rout);
+    [W(:,i),routlast(i)] = perceptron_learning_keep_last_solution(X,Xp,f,rj,int_noise);
 end
 delete(gcp)
-save(sprintf('/scratch/zhang.chi9/perceptron/data/perceptron_suc_prob/N_%d_alpha_%f.mat',N,alpha))
+save(sprintf('/scratch/zhang.chi9/perceptron/data/perceptron_suc_prob/N_%d_alpha_%f_intNoise_%f.mat',N,alpha,int_noise))
 
